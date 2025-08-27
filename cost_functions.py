357228a4e3792022be3939cfb824b6846bd32ff5
Aiden Hong <aidenhh@uci.edu>
2025-08-26 20:13:03 -0700

    -Added epsilon of 1e-15 to prevent ln(0) errors

--- cost_functions.py
+++ cost_functions.py
@@ -4,7 +4,12 @@ def get_CCE(label : int, output_neurons : np.array) -> float:
     """
     Ideal values hot-coded as follows (top - down): [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     Loss here uses CCE, which is equivalent to -1 * sum(ideal * ln(real))
+    An epsilon of 10 **(-15) is used to avoid ln(0) errors.
     """
+    # Add epsilon
+    epsilon = 10 **(-15)
+    output_neurons += epsilon
+
     # Instantiate one hot-coded vector
     hot_encoded = np.array([int(i == label) for i in range(10)]).reshape(-1, 1)
 
